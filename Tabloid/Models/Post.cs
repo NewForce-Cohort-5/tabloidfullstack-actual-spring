0571f05e63455af7770c237aa2d5cb627097c0a8
Nima-ShSh <nimashsh21@gmail.com>
2022-03-22 15:34:00 -0400

    commit before checking Jordans code

---
+++ Tabloid/Models/Post.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Gifter.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string ImageLocation { get; set; }
+
+        [Required]
+        public DateTime CreateDateTime { get; set; }
+
+        [Required]
+        public DateTime PublishDateTime { get; set; }
+
+        [Required]
+        public int UserProfileId { get; set; }
+
+        //isApproved
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public int UserProfileId { get; set; }
+
+
+        //[Required]
+        //public string ImageUrl { get; set; }
+
+        //public string Caption { get; set; }
+
+        //public UserProfile UserProfile { get; set; }
+
+        //public List<Comment> Comments { get; set; } = new List<Comment>();
+
+    }
+}
---
+++ Tabloid/Repositories/IPostRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using TabloidMVC.Models;
+
+
+using TabloidFullStack.Models;
+using TabloidFullStack.Utils;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace TabloidFullStack.Repositories
+{
+    public interface IPostRepository
+    {
+        //void Add(Post post);
+        //void Delete(int id);
+        List<Post> GetAll();
+        //List<Post> GetAllWithComments();
+        //Post GetById(int id);
+        //void Update(Post post);
+    }
+}
