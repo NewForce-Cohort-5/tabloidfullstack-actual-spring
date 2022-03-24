7b833468d07a3ba965311400b28914b5df0c1fcf
Nima-ShSh <nimashsh21@gmail.com>
2022-03-24 11:34:18 -0400

    commit before test

--- Tabloid/client/src/components/posts/Post.js
+++ Tabloid/client/src/components/posts/Post.js
@@ -4,6 +4,11 @@ import { Link } from "react-router-dom";
 
 const Post = ({ postProp }) => {
     
+
+  // let date = (userProfile.publishDateTime);
+  // let array = date.split('-').reverse().join('.')
+  // var postDate = (array[12] + array[13] + "/" + array[0] + array[1]+ "/"+ array[15]+ array[16]+ array[17]+ array[18]);
+
   return (
     <Card className="m-4">
       <CardImg top src={postProp.userProfile.imageLocation}  /> 
@@ -11,9 +16,20 @@ const Post = ({ postProp }) => {
       <p> Post title: <Link to={`/posts/${postProp.id}`}><strong>{postProp.title}</strong> </Link>
       </p>
       <p className="text-left px-0">Posted by: {postProp.userProfile.fullName}</p>
+      <p className="text-left px-0">Date posted: {postProp.publishDateTime}</p>
+      <p className="approved">{}</p>
       </CardBody>
     </Card>
   );
 };
 export default Post;
+
+
+
+
+
+
+
+
+
 
