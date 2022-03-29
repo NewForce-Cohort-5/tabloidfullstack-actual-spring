f444b4e857b596b29a810ef8f92d3d5745a5542d
Jaytwy24 <jtwy24@gmail.com>
2022-03-29 15:35:22 -0400

    update button

--- Tabloid/client/src/components/posts/PostDetail.js
+++ Tabloid/client/src/components/posts/PostDetail.js
@@ -3,11 +3,15 @@ import { ListGroup, ListGroupItem } from "reactstrap";
 import { PostContext } from "../../providers/PostProvider"; 
 import { Link, useParams } from "react-router-dom";
 import { Post } from "./Post";
+import { Button } from "bootstrap";
+import { useNavigate} from "react-router-dom";
+
 
 export const PostDetail = () => {
   const [singlePost, setPost] = useState({});
   const { GetPostsById } = useContext(PostContext);
   const { id } = useParams();
+  const navigate = useNavigate()
 
   useEffect(() => {
     GetPostsById(id).then(setPost);
@@ -23,11 +27,15 @@ export const PostDetail = () => {
               <ListGroupItem>{c.message}</ListGroupItem>
             ))}
           </ListGroup> */}
-          <Link to={`/post/comments/${singlePost.id}`}><strong>{singlePost.title}</strong> </Link>
+          <strong>{singlePost.title}</strong>
           <div><img src={singlePost.imageLocation} alt="post-image"></img></div>
           <p>{singlePost.content}</p>
           <strong> Published on: {new Date(singlePost.publishDateTime).toLocaleDateString('en-us')}</strong>
           <strong> Author: {singlePost.userProfile?.displayName}</strong>
+          <div>  <button className="btn btn-primary" outline onClick={() => navigate(`/post/comments/${singlePost.id}`)}>
+    View Comments
+  </button></div>
+        
         </div>
       </div>
     </div>
